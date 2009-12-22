35b1b461313b910186c8fbfb6a6368731c5242b4
Tamas Cservenak <tamas@cservenak.net>
2009-12-22 18:16:17 +0100

    Added javadoc

--- nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/RubyGateway.java
+++ nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/RubyGateway.java
@@ -15,13 +15,40 @@ import org.sonatype.nexus.ruby.MavenArtifact;
  */
 public interface RubyGateway
 {
+    /**
+     * Just builds proper GEM filename out from information in POM object.
+     * 
+     * @param pom Pom of the artifact.
+     * @return
+     */
     String getGemFileName( Model pom );
 
+    /**
+     * Does Maven2 artifact conversion into Gem and writes the Gem to the target file. The file written to target should
+     * be a complete Ruby Gem.
+     * 
+     * @param mart
+     * @param target
+     * @throws IOException
+     */
     void createGemFromArtifact( MavenArtifact mart, File target )
         throws IOException;
 
+    /**
+     * Creates a Gem::Specification from the Maven2 artifact Pom and writes it into the designated target File as Yaml
+     * serialized file (for latter indexer use, this is the gemspec of "lazy" Gems).
+     * 
+     * @param pom
+     * @param target
+     * @throws IOException
+     */
     void createAndWriteGemspec( Model pom, File target )
         throws IOException;
 
+    /**
+     * Invokes "gem generate_index --directory=${basedir}". Should do the same as the CLI command written above.
+     * 
+     * @param basedir
+     */
     void gemGenerateIndexes( File basedir );
 }
