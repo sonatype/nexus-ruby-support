4e9bd3377f795dffeee2055ce7e688fb6725a4ce
Tamas Cservenak <tamas@cservenak.net>
2010-03-27 14:52:58 +0100

    Adding null representer.

--- nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/gem/yaml/MappingRepresenter.java
+++ nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/gem/yaml/MappingRepresenter.java
@@ -4,7 +4,9 @@ import org.sonatype.nexus.ruby.gem.GemDependency;
 import org.sonatype.nexus.ruby.gem.GemRequirement;
 import org.sonatype.nexus.ruby.gem.GemSpecification;
 import org.sonatype.nexus.ruby.gem.GemVersion;
+import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Represent;
 import org.yaml.snakeyaml.representer.Representer;
 
 /**
@@ -19,9 +21,20 @@ public class MappingRepresenter
     {
         super();
 
+        this.nullRepresenter = new RepresentNull();
+
         this.addClassTag( GemSpecification.class, new Tag( "!ruby/object:Gem::Specification" ) );
         this.addClassTag( GemDependency.class, new Tag( "!ruby/object:Gem::Dependency" ) );
         this.addClassTag( GemRequirement.class, new Tag( "!ruby/object:Gem::Requirement" ) );
         this.addClassTag( GemVersion.class, new Tag( "!ruby/object:Gem::Version" ) );
+    }
+
+    private class RepresentNull
+        implements Represent
+    {
+        public Node representData( Object data )
+        {
+            return representScalar( Tag.NULL, "null" );
+        }
     }
 }
