1e13addf18a9c36f3945b1b68b09940bd5d5deea
Kristian Meier <m.kristian@web.de>
2012-08-14 13:04:39 +0530

    do not need extra repo-type

--- nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/RubyHostedRepository.java
+++ nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/RubyHostedRepository.java
@@ -1,9 +1,7 @@
 package org.sonatype.nexus.plugins.ruby;
 
-import org.sonatype.nexus.plugins.RepositoryType;
 import org.sonatype.nexus.proxy.repository.HostedRepository;
 
-@RepositoryType( pathPrefix="rubygems" )
 public interface RubyHostedRepository
     extends RubyRepository, HostedRepository
 {
