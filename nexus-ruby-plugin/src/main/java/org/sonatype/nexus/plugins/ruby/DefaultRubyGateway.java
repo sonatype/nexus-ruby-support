7a46bbfbde7c4f0a1ed094fe6430bed51577102d
Kristian Meier <m.kristian@web.de>
2012-08-17 18:19:22 +0530

    removed all shadow related code

--- nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/DefaultRubyGateway.java
+++ nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/DefaultRubyGateway.java
@@ -1,16 +1,9 @@
 package org.sonatype.nexus.plugins.ruby;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.logging.Logger;
-import org.codehaus.plexus.util.FileUtils;
-
-import de.saumya.mojo.gems.MavenArtifact;
-import de.saumya.mojo.gems.MavenArtifactConverter;
-import de.saumya.mojo.gems.spec.GemSpecification;
-import de.saumya.mojo.gems.spec.GemSpecificationIO;
 
 /**
  * This is the naive implementation of the RubyGateway, that not uses JRuby to do the work, and is incomplete.
