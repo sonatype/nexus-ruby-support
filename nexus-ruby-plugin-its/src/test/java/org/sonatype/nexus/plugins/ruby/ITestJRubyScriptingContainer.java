cfb3be95b75d235729cec4be794d063655ae2ff2
Kristian Meier <m.kristian@web.de>
2012-09-17 15:39:23 +0530

    setup Gemfile location as well

--- nexus-ruby-plugin-its/src/test/java/org/sonatype/nexus/plugins/ruby/ITestJRubyScriptingContainer.java
+++ nexus-ruby-plugin-its/src/test/java/org/sonatype/nexus/plugins/ruby/ITestJRubyScriptingContainer.java
@@ -1,10 +1,16 @@
 package org.sonatype.nexus.plugins.ruby;
 
+import java.io.File;
+
 import org.sonatype.nexus.ruby.TestJRubyScriptingContainer;
 
 class ITestJRubyScriptingContainer extends TestJRubyScriptingContainer
 {
     ITestJRubyScriptingContainer(){
         super( "target/rubygems" );
+    }
+    
+    ITestJRubyScriptingContainer( File gemfile ){
+        super( "target/rubygems", gemfile.getAbsolutePath() );
     }
 }
