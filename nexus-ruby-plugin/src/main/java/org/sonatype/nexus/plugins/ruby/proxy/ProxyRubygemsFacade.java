1043740aa962604dd9bc40a5fbb435006443aad6
Christian Meier <m.kristian@web.de>
2014-01-14 11:19:10 +0000

    allow to delete files on rubygems proxy

--- nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/proxy/ProxyRubygemsFacade.java
+++ nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/proxy/ProxyRubygemsFacade.java
@@ -2,11 +2,19 @@ package org.sonatype.nexus.plugins.ruby.proxy;
 
 import org.sonatype.nexus.plugins.ruby.RubyRepository;
 import org.sonatype.nexus.plugins.ruby.fs.AbstractRubygemsFacade;
+import org.sonatype.nexus.plugins.ruby.fs.RubyLocalRepositoryStorage;
+import org.sonatype.nexus.proxy.item.StorageFileItem;
 import org.sonatype.nexus.ruby.RubygemsGateway;
 
 public class ProxyRubygemsFacade extends AbstractRubygemsFacade {
     
     public ProxyRubygemsFacade(RubygemsGateway gateway, RubyRepository repository) {
         super(gateway, repository);
+    }
+    
+    @Override
+    public boolean removeGem( RubyLocalRepositoryStorage storage, StorageFileItem gem )
+    {
+        return true;
     }
 }
