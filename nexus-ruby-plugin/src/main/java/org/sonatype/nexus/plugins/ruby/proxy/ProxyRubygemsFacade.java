a2cd94346b45d378e50632b8c784c139415f7f65
Christian Meier <m.kristian@web.de>
2014-01-20 15:36:59 +0000

    do not through exception when addGem on the proxy - just do nothing

--- nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/proxy/ProxyRubygemsFacade.java
+++ nexus-ruby-plugin/src/main/java/org/sonatype/nexus/plugins/ruby/proxy/ProxyRubygemsFacade.java
@@ -3,7 +3,10 @@ package org.sonatype.nexus.plugins.ruby.proxy;
 import org.sonatype.nexus.plugins.ruby.RubyRepository;
 import org.sonatype.nexus.plugins.ruby.fs.AbstractRubygemsFacade;
 import org.sonatype.nexus.plugins.ruby.fs.RubyLocalRepositoryStorage;
+import org.sonatype.nexus.plugins.ruby.fs.RubygemFile;
+import org.sonatype.nexus.proxy.LocalStorageException;
 import org.sonatype.nexus.proxy.item.StorageFileItem;
+import org.sonatype.nexus.proxy.storage.UnsupportedStorageOperationException;
 import org.sonatype.nexus.ruby.RubygemsGateway;
 
 public class ProxyRubygemsFacade extends AbstractRubygemsFacade {
@@ -17,4 +20,14 @@ public class ProxyRubygemsFacade extends AbstractRubygemsFacade {
     {
         return true;
     }
+
+    @Override
+    public RubygemFile addGem( RubyLocalRepositoryStorage storage,
+                               StorageFileItem gem )
+            throws UnsupportedStorageOperationException, LocalStorageException
+    {
+        // nothing to do and null should be sufficient
+        return null;
+    }
+
 }
