5f93e5af36fa5d411b1737907bb97d7204baab1a
Christian Meier <m.kristian@web.de>
2014-05-15 12:25:58 +0100

    use concrete version

--- run-all-its.sh
+++ run-all-its.sh
@@ -4,6 +4,6 @@ mvn clean install
 
 cd nexus-ruby-plugin-its
 
-mvn -P run-its clean install
+mvn -P run-its clean install -Dit.nexus.version=2.7.2-03
 mvn -P run-its clean install -Dit.nexus.version=2.6.4-02
 mvn -P run-its clean install -Dit.nexus.version=2.8.0-05
