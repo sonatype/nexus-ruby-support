0f83656aa47cbe896ff2d730cd8aa76784f4b810
Christian Meier <m.kristian@web.de>
2014-07-28 16:33:32 +0100

    added 2.9.x version

--- run-all-its.sh
+++ run-all-its.sh
@@ -7,3 +7,4 @@ cd nexus-ruby-plugin-its
 mvn -P run-its clean install -Dit.nexus.version=2.7.2-03
 mvn -P run-its clean install -Dit.nexus.version=2.6.4-02
 mvn -P run-its clean install -Dit.nexus.version=2.8.0-05
+mvn -P run-its clean install -Dit.nexus.version=2.9.0-SNAPSHOT
