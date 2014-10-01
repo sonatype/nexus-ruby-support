3f6df40f4f8575c851f34714633774c65884ab3b
Christian Meier <m.kristian@web.de>
2014-10-01 16:58:24 +0100

    bump release version

--- run-all-its.sh
+++ run-all-its.sh
@@ -6,5 +6,7 @@ cd nexus-ruby-plugin-its
 
 mvn -P run-its clean install -Dit.nexus.version=2.7.2-03
 mvn -P run-its clean install -Dit.nexus.version=2.6.4-02
-mvn -P run-its clean install -Dit.nexus.version=2.8.0-05
-mvn -P run-its clean install -Dit.nexus.version=2.9.0-SNAPSHOT
+mvn -P run-its clean install -Dit.nexus.version=2.8.1-01
+mvn -P run-its clean install -Dit.nexus.version=2.9.2-01
+mvn -P run-its clean install -Dit.nexus.version=2.10.0-02
+#mvn -P run-its clean install -Dit.nexus.version=2.9.0-SNAPSHOT
